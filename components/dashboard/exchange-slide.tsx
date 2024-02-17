a6a648a84da25114462d3b13abd2e992e7349a07
Nedxlab <odoc047@gmail.com>
2024-02-17 21:05:50 +0100

    part of the dashboard, more to come soon

---
+++ components/dashboard/exchange-slide.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { AdvancedChart, MiniChart } from "react-tradingview-embed";
+
+function ExchangeSlide({ cryptoPair }: any) {
+  return (
+    <MiniChart
+      widgetProps={{
+        colorTheme: "dark",
+        symbol: cryptoPair,
+        width: "100%",
+        height: "100%",
+        locale: "en",
+        dateRange: "1D",
+        underLineColor: "rgba(101, 101, 101, 0.36)",
+        isTransparent: false,
+        autosize: false,
+        noTimeScale: true,
+      }}
+    />
+  );
+}
+
+export default ExchangeSlide;
