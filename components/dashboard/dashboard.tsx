868bcbcaad0af5da4cacc1b69d624fa4af3766e0
msilucifer <tammyhan0830@gmail.com>
2024-03-30 16:29:19 -0400

    fix: finished adding four dashboard pages mainly

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,11 +1,16 @@
+"use client";
 import React from "react";
 import Exchange from "./exchange";
 import CardSlider from "./cardSlider";
+import CustomChart from "./customChart";
+import Second from "./second";
+import First from "./first";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col w-screen px-7 py-4">
-      <Exchange />
+    <div className="flex flex-col w-screen px-7 bg-[#000] h-screen">
+      <First />
+      <Second />
       <CardSlider />
     </div>
   );
