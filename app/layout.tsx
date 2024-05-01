5fe4d2c70305fc0563123b1ca2b309d3c1bf8bf3
trmvenus <tammyhan0830@gmail.com>
2024-05-01 16:43:30 -0700

    Login Page Responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,44 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import { Poppins } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar'
-import NavbarAuth from '@/components/navbar/navbarAuth'
-import NavbarDashboard from '@/components/navbar/navbarDashboard'
-// Import css files
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import { AppWrapper } from '@/context/AddContext'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 
-const microsoft = localFont({ src: '../public/fonts/chinese.msyh.ttf' })
+import Navbar from "@/components/navbar";
+import NavbarAuth from "@/components/navbar/navbarAuth";
+import NavbarDashboard from "@/components/navbar/navbarDashboard";
+import { AppWrapper } from "@/context/AddContext";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./globals.css";
+
+const microsoft = localFont({ src: "../public/fonts/chinese.msyh.ttf" });
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
-  title: 'GTX',
-  description: 'gtx',
-}
+  title: "GTX",
+  description: "gtx",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={poppins.className + ' bg-black'}>
+    <html lang="en">
+      <body className={poppins.className + " bg-black"}>
         <AppWrapper>
           <Navbar />
           {children}
         </AppWrapper>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"
+          defer
+        ></script>
       </body>
     </html>
-  )
+  );
 }
