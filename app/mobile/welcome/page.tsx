cdab047aa2b1ecb11e0519d311f713a3334150c1
trmvenus <tammyhan0830@gmail.com>
2024-05-08 02:50:40 -0400

    fix welcome page add font classname

--- app/mobile/welcome/page.tsx
+++ app/mobile/welcome/page.tsx
@@ -3,13 +3,11 @@ import { AuthContext } from "@/context/AddContext";
 import React, { useContext, useEffect } from "react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
-import localFont from "next/font/local";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-const SFPro = localFont({ src: "../../../public/fonts/SFPro.otf" });
 
 const Welcome = () => {
   const appContext = useContext(AuthContext);
@@ -24,7 +22,7 @@ const Welcome = () => {
         <h1 className=" text-[32px]/[48px] text-white font-bold">
           Welcome to GTX
         </h1>
-        <p className={`text-sm text-white/[.7] ${SFPro.className} font-normal`}>
+        <p className={`text-sm text-white/[.7] font-SFPro font-normal`}>
           All your crypto transactions in one place! Track coins, aadd
           portfolios, buy & sell.
         </p>
