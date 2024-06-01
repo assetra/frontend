6d87609c8e0889459d778a360a0ff76647fb53cc
claudezion <claudezion@icloud.com>
2024-06-01 19:04:14 +0530

    fixed the route to the home page and removed the pre existed /homepage route

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import Login from "@/components/auth/login";
-import Image from "next/image";
+import About from "@/components/homepage/About";
+import DigitalAssets from "@/components/homepage/DigitalAssets";
+import Divider from "@/components/homepage/Divider";
+import Footer from "@/components/homepage/Footer";
+import Team from "@/components/homepage/Team";
+import Crypto from "@/components/homepage/Crypto";
 
 export default function Home() {
   return (
     <>
-      <Login />
+      <DigitalAssets />
+      <Divider />
+      <About />
+      <Crypto />
+      <Team />
+      <Footer />
     </>
   );
 }
