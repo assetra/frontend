a8617ca172206f6dd3b0b3f6ea108e0b0e4c947c
claudezion <claudezion@icloud.com>
2024-07-01 03:15:21 +0530

    added Open Graph meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,55 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import { Poppins } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar'
-import NavbarAuth from '@/components/navbar/navbarAuth'
-import NavbarDashboard from '@/components/navbar/navbarDashboard'
-// Import css files
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import { AppWrapper } from '@/context/AddContext'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const microsoft = localFont({ src: '../public/fonts/chinese.msyh.ttf' })
+import Navbar from "@/components/navbar";
+import { ConnectWallet } from "@/components/wallet/ConnectWallet";
+import { ThemeProvider } from "@/context/ThemeContext";
+import { AppWrapper } from "@/context/AddContext";
+import { Connect } from "@/components/wallet/Connect";
+import { ConnectError } from "@/components/wallet/connectionError";
+import { Retry } from "@/components/wallet/Retry";
+import { WalletDetails } from "@/components/wallet/WalletDetails";
+
+const microsoft = localFont({ src: "../public/fonts/chinese.msyh.ttf" });
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
-  title: 'GTX',
-  description: 'gtx',
-}
+  title: {
+    template: "%s | GTX",
+    default: "GTX - Your Customizable Crypto Trading Platform",
+  },
+  description:
+    "GTX is a top-tier crypto trading platform providing advanced customization features, designed to meet the unique needs of traders.",
+  metadataBase: new URL("https://www.gtx.com.co"),
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={poppins.className + ' bg-black'}>
-        <AppWrapper>
-          <Navbar />
-          {children}
-        </AppWrapper>
+    <html lang="en">
+      <body className={poppins.className}>
+        <ThemeProvider>
+          <AppWrapper>
+            <Navbar />
+            {children}
+            <ConnectWallet />
+            <Connect />
+            <ConnectError />
+            <Retry />
+            <WalletDetails />
+          </AppWrapper>
+        </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
