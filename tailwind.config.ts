23cd59a3a0cf66583da6214eb4e504e353cce555
claudezion <claudezion@icloud.com>
2024-07-01 03:13:56 +0530

    Adding Dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      height: {
+        "calc-100vh-minus-3rem": "calc(100vh - 3rem)",
       },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-export default config
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "forest",
+      "aqua",
+      "black",
+      "luxury",
+      "business",
+      "lemonade",
+      "night",
+      "coffee",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+};
+export default config;
