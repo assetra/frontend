0398812b0622235c0c490e690d083cf8ecae3c19
claudezion <claudezion@icloud.com>
2024-07-09 20:13:51 +0530

    fix: Add fallback for WalletConnect project ID to avoid build errors
    - Added a default project ID using an OR condition to ensure that the app builds correctly even if NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID is not defined.
    - Included a comment explaining the rationale behind using the OR condition.

--- components/wallet/wagmi.ts
+++ components/wallet/wagmi.ts
@@ -1,4 +1,5 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+// rainbowkitConfig.ts
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
   arbitrum,
   base,
@@ -6,18 +7,23 @@ import {
   optimism,
   polygon,
   sepolia,
-} from 'wagmi/chains';
+} from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: 'GTX',
-  projectId:process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
+  appName: "GTX",
+  projectId:
+    (process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string) ||
+    "4840555280a77046d25240b84fc788af",
+  // Using an OR condition to provide a default value for the WalletConnect project ID
+  // This ensures that if NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID is not defined,
+  // the application will still have a valid project ID to avoid build errors.
   chains: [
     mainnet,
     polygon,
     optimism,
     arbitrum,
     base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
   ],
   ssr: true,
 });
