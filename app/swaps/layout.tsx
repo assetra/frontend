27c336518237a7693caa21a3dfd98d7b793df66c
Yash Jain <yashj8858@gmail.com>
2024-07-19 12:02:12 +0530

    swaps

---
+++ app/swaps/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import "../globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+
+import {
+  Web3ReactHooks,
+  Web3ReactProvider,
+  initializeConnector,
+} from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
+
+import { Toaster } from "../../components/ui/toaster";
+
+export const [metaMask, hooks] = initializeConnector<MetaMask>(
+  (actions) => new MetaMask({ actions }),
+);
+
+const connectors: [MetaMask, Web3ReactHooks][] = [[metaMask, hooks]];
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+
+      <body className="bg-black">
+
+        <Web3ReactProvider connectors={connectors}>
+          <main>{children}</main>
+        </Web3ReactProvider>
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
---
+++ app/swaps/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "../../components/ui/Hero";
+
+export default function Home() {
+  return (
+    <div className="text-white">
+      <Hero />
+    </div>
+  );
+}
